140f77b6f9119dc7a762aba8df8d62638d351d24
khaleed opeloyeru <kopeloye@my.centennialcollege.ca>
2025-02-23 22:03:58 -0500

    Updated greeting message based on time

--- src/main/java/com/example/comp367/App.java
+++ src/main/java/com/example/comp367/App.java
@@ -5,9 +5,14 @@ import java.time.LocalTime;
 public class App {
     public static void main(String[] args) {
         LocalTime currentTime = LocalTime.now();
+        int hour = currentTime.getHour();
 
-        String greeting = "Welcome to COMP367";
-
+        String greeting;
+        if (hour < 12) {
+            greeting = "Good morning, Khaleed Opeloyeru, Welcome to COMP367, have a good day";
+        } else {
+            greeting = "Good afternoon, Khaleed Opeloyeru, Welcome to COMP367, how is your day going?";
+        }
         System.out.println(greeting);
     }
 }
